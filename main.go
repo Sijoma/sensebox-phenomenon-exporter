c38abfbd514b203acab99574c0813b4777db6f28
Simon Zengerling <simonschulte1991@gmail.com>
2021-05-25 21:21:59 +0200

    Update main.go

--- main.go
+++ main.go
@@ -63,12 +63,9 @@ func GetSensebox(senseboxUrl string) Sensebox {
 		fmt.Println(err)
 	}
 	defer resp.Body.Close()
-	//body, _ := io.ReadAll(resp.Body)
 
 	var s Sensebox
 
-	// Try to decode the request body into the struct. If there is an error,
-	// respond to the client with the error message and a 400 status code.
 	json.NewDecoder(resp.Body).Decode(&s)
 	return s
 }
